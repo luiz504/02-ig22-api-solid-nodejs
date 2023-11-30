70666767924fb2aef1f60fb12a4e442a77478061
Luiz Bueno <luiz504@hotmail.com>
2023-11-30 09:26:31 -0300

    chore(tests): config isalated test suites

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,14 +1,37 @@
+import { PrismaClient } from '@prisma/client'
+import 'dotenv/config'
+import { execSync } from 'node:child_process'
+import { randomUUID } from 'node:crypto'
 import { Environment } from 'vitest'
 
+const prisma = new PrismaClient()
+function generateDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('DATABASE_URL environment variable must be provided.')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+  return url.toString()
+}
 export default <Environment>{
   name: 'prisma',
   transformMode: 'web',
   async setup() {
-    console.log('Setup') //eslint-disable-line
+    const schema = randomUUID()
+    const databaseURL = generateDatabaseURL(schema)
+
+    process.env.DATABASE_URL = databaseURL
+
+    execSync('npx prisma migrate deploy')
 
     return {
-      teardown() {
-        console.log('Teardown') //eslint-disable-line
+      async teardown() {
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        )
+        await prisma.$disconnect()
       },
     }
   },
