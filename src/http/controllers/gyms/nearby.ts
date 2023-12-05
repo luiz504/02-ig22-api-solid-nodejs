41c45ab230b27345d2653bb054234b83a6f842c5
Luiz Bueno <luiz504@hotmail.com>
2023-12-05 08:39:55 -0300

    chore(controllers): create routes/conntrollers

--- src/http/controllers/gyms/nearby.ts
+++ src/http/controllers/gyms/nearby.ts
@@ -18,7 +18,7 @@ export async function nearby(request: FastifyRequest, reply: FastifyReply) {
     ),
   })
 
-  const { latitude, longitude } = nearbyGymsQuerySchema.parse(request.body)
+  const { latitude, longitude } = nearbyGymsQuerySchema.parse(request.params)
 
   const fetchNearbyGymUseCase = makeFetchNearbyGymInUseCase()
   const { gyms } = await fetchNearbyGymUseCase.execute({
