c7c5eb3dce9cd71996140e6488098b0d5c2149fa
Luiz Bueno <luiz504@hotmail.com>
2023-12-07 09:01:20 -0300

    feat(refreshToken): implementing refresh token

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import fastify from 'fastify'
 import fastifyJwt from '@fastify/jwt'
+import fastifyCookie from '@fastify/cookie'
 import { ZodError } from 'zod'
 
 import { env } from './env'
@@ -10,7 +11,12 @@ import { checkInsRoutes } from './http/controllers/check-ins/routes'
 
 export const app = fastify()
 
-app.register(fastifyJwt, { secret: env.JWT_SECRET })
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  cookie: { cookieName: 'refreshToken', signed: false },
+  sign: { expiresIn: '10m' },
+})
+app.register(fastifyCookie)
 
 // Routes
 app.register(usersRoutes)
--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -2,10 +2,13 @@ import { FastifyInstance } from 'fastify'
 
 import { profile, authenticate, register } from '.'
 import { verifyJWT } from '~/http/middlewares/verify-jwt'
+import { refresh } from './refresh'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  app.patch('/token/refresh', refresh)
 
   //* * Authenticated */
   app.get('/me', { onRequest: [verifyJWT] }, profile)
