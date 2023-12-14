e0968285827040ca08311f4728435fe6a9e51cb2
Luiz <luiz504@hotmail.com>
2023-12-14 08:18:08 -0300

    ci: test envs workaround

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -6,11 +6,17 @@ import { Environment } from 'vitest'
 
 const prisma = new PrismaClient()
 function generateDatabaseURL(schema: string) {
-  if (!process.env.DATABASE_URL) {
+  let envURL = process.env.DATABASE_URL
+
+  if (process.env.NODE_ENV === 'ci') {
+    envURL = 'postgresql://docker:docker@localhost:5432/apisolid?schema=public'
+  }
+
+  if (!process.env.DATABASE_URL && process.env.NODE_ENV !== 'ci') {
     throw new Error('DATABASE_URL environment variable must be provided.')
   }
 
-  const url = new URL(process.env.DATABASE_URL)
+  const url = new URL(envURL || '')
 
   url.searchParams.set('schema', schema)
   return url.toString()
@@ -18,9 +24,10 @@ function generateDatabaseURL(schema: string) {
 
 export default <Environment>{
   name: 'prisma',
-  transformMode: 'web',
+  transformMode: 'ssr',
   async setup() {
     const schema = randomUUID()
+
     const databaseURL = generateDatabaseURL(schema)
 
     process.env.DATABASE_URL = databaseURL
